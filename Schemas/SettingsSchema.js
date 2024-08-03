a5b92c999c90876f44ee2afda9fb70cbd3e4b332
weebthedev <167898252+weebthedev@users.noreply.github.com>
2024-08-03 15:15:07 -0400

    Update SettingsSchema.js

--- Schemas/SettingsSchema.js
+++ Schemas/SettingsSchema.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const ServerSettingsSchema = new mongoose.Schema({
   serverId: { type: String, required: true, unique: true },
   prefix: { type: String, required: true, default: '$' },
-  MuteRole: { type: String, required: true }
 });
 
 module.exports = mongoose.model('ServerSettings', ServerSettingsSchema)
